3ad4d7fd145255594eebaf2414abf0f33a8e749a
OluCole <coleseyi19@gmail.com>
2025-02-04 08:47:01 -0500

    Create falling apple3.py

---
+++ falling apple3.py
@@ -0,0 +1,148 @@
+#   a123_apple_1.py
+import random
+import turtle as trtl
+
+#-----setup-----
+apple_image = "apple.gif" # Store the file name of your shape
+
+
+
+wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
+wn.bgpic("background.gif")
+wn.addshape(apple_image) # Make the screen aware of the new file
+letters = ["a","s","d","f"]
+def make_turtles(letters):
+  turtles = []
+  for _ in letters: 
+    t = trtl.Turtle()
+    t.penup()
+    draw_apple(t)
+    t.goto(random.randint(-100, 100), random.randint(-10, 90))
+    turtles.append(t)
+
+drawer = trtl.Turtle()
+
+
+apple = trtl.Turtle()
+apple.penup()
+# pear = trtl.Turtle()
+
+#-----functions-----
+# given a turtle, set that turtle to be shaped by the image file
+def draw_apple(active_apple):
+  active_apple.shape(apple_image)
+  wn.update()
+
+# def draw_letters(letters, turtles):
+#   drawers = []
+#   for i in range(len(turtles)):
+#     drawer = trtl.Turtle()
+#     drawer.penup()
+#     drawer.goto(trtl.xcor()-18,trtl.ycor()-35)
+#     drawer.write(letters[i], font = ("Arial", 40, "bold"))
+#     drawers.append(drawer)
+
+
+
+def a_pressed():
+  print("A Key pressed.")
+  index = 0
+  apple_fall(index)
+  erase_letter(index)
+
+def s_pressed():
+  print("S Key pressed.")
+  index = 1
+  apple_fall(index)
+  erase_letter(index)
+
+def d_pressed():
+  print("D Key pressed.")
+  index = 2
+  apple_fall(index)
+  erase_letter(index)
+
+def f_pressed():
+  print("F Key pressed.")
+  index = 3
+  apple_fall(index)
+  erase_letter(index)
+  
+def apple_fall(index):
+  global apple_turtles
+  apple = apple_turtles[index]
+  x = apple.xcor()
+  y = apple.ycor()
+  apple.goto(x, -140)
+
+def erase_letter(index):
+  global drawer_turtles
+  drawer = drawer_turtles[index]
+  drawer.clear()
+  
+# def apple_fall():
+#   global apple
+#   erase_an_A()
+#   x = apple_turtles.xcor()
+  # global apple
+  # erase_an_A()
+  # x = apple.xcor()
+  # y = apple.ycor()
+  # apple.goto(x,y-140)
+
+def make_apple_turtles(letters):
+  apple_turtles = []
+  for _ in range(len(letters)):
+    apple_turtle = trtl.Turtle()
+    apple_turtle.penup()
+    apple_turtle.goto(random.randint(-120, 120), random.randint(-10, 40))
+    draw_apple(apple_turtle)
+    apple_turtles.append(apple_turtle)
+  return apple_turtles
+
+
+
+def draw_letters(letters, apple_turtles):
+  drawer_turtles = []
+  for i in range(len(letters)):
+    letter = letters[i]
+    drawer =  trtl.Turtle()
+    drawer.hideturtle()
+    drawer.penup()
+    apple_turtle = apple_turtles[i]
+    drawer.goto(apple_turtle.xcor()-18, apple_turtle.ycor()-40)
+    drawer.color("white")
+    drawer.write(letter.upper(), font=("Arial", 40, "bold"))
+    drawer_turtles.append(drawer)
+  return drawer_turtles
+
+def draw_an_A(drawer):
+  drawer.hideturtle()
+  drawer.goto(-13,-35)
+  drawer.color("black")
+  drawer.write("A", font=("Arial", 30, "bold"))
+
+def erase_an_A():
+  global drawer
+  drawer.clear()
+
+def letter(apple):
+  pass
+
+#-----function calls-----
+apple_turtles = make_apple_turtles(letters)
+drawer_turtles = draw_letters(letters, apple_turtles)
+
+
+wn.onkeypress(a_pressed, "a")
+wn.onkeypress(s_pressed, "s")
+wn.onkeypress(d_pressed, "d")
+wn.onkeypress(f_pressed, "f")
+# draw_pear(pear)
+# draw_apple(apple)
+# draw_an_A(drawer)
+# wn.onkeypress(apple_fall, "a")
+
+wn.listen()
+wn.mainloop()
